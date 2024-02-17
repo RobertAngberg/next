b9e9982d4b1f60a7285c395d82bc54f40e95db8f
RobertAngberg <124473123+RobertAngberg@users.noreply.github.com>
2024-02-17 03:56:09 +0100

    asdf

---
+++ app/Card.tsx
@@ -0,0 +1,8 @@
+export default function Card() {
+  return (
+    <div className="w-1/3 items-center m-auto mx-8 max-w-5xl bg-cyan-950 border-4 rounded-xl">
+      <div className="text-4xl font-bold p-5 text-white">I'm gay</div>
+      <div className="bg-white text-sky-950 font-bold text-2xl p-6">asdf</div>
+    </div>
+  );
+}
