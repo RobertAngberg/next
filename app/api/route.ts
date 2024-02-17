861722d2f2836cdace899e51f16e831f337b9536
RobertAngberg <124473123+RobertAngberg@users.noreply.github.com>
2024-02-17 14:33:41 +0100

    Refactor API route and update Home component

--- app/api/route.ts
+++ app/api/route.ts
@@ -2,17 +2,17 @@ import { sql } from "@vercel/postgres";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url);
-  const petName = searchParams.get("petName");
-  const ownerName = searchParams.get("ownerName");
+  // const { searchParams } = new URL(request.url);
 
+  // const petName = searchParams.get("petName");
+  // const ownerName = searchParams.get("ownerName");
   // try {
   //   if (!petName || !ownerName) throw new Error("Pet and owner names required");
   //   await sql`INSERT INTO Pets (Name, Owner) VALUES (${petName}, ${ownerName});`;
-  // } catch (error) {
-  //   return NextResponse.json({ error }, { status: 500 });
+  // } catch (error: any) {
+  //   return NextResponse.json({ error: error.message }, { status: 500 });
   // }
 
   const pets = await sql`SELECT * FROM Pets;`;
-  return NextResponse.json({ pets: pets.rows }, { status: 200 });
+  return NextResponse.json({ pets }, { status: 200 });
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,37 @@
+import { useState } from "react";
 import Card from "./Card";
 
-export default function Home() {
+type Pet = {
+  name: string;
+  owner: string;
+};
+
+export default async function Home() {
+  // const response = await fetch("https://dummyjson.com/products/2");
+  // const data = await response.json();
+
+  const response = await fetch("http://localhost:3000/api");
+  // const asdf = await response.json();
+  // console.log(asdf);
+  const { pets: fetchedPets } = await response.json(); // Destructuring here
+  console.log(fetchedPets);
+
   return (
     <main className="items-center p-24 text-center flex">
       <Card />
       <Card />
       <Card />
+
+      <h1>Pets List</h1>
+      <ul>
+        {fetchedPets &&
+          fetchedPets.map &&
+          fetchedPets.map((pet: Pet, index: number) => (
+            <li key={index}>
+              {pet.name} - Owner: {pet.owner}
+            </li>
+          ))}
+      </ul>
     </main>
   );
 }
