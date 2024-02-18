9acf7af3f8dcb00d9f4374c93ea7a6dab5066e7c
RobertAngberg <124473123+RobertAngberg@users.noreply.github.com>
2024-02-18 01:43:22 +0100

    Refactor Home component and add HomeChart component

---
+++ app/HomeChart.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import React from "react";
+import "chart.js/auto";
+import { Bar } from "react-chartjs-2";
+
+type RowData = {
+  id: number;
+  datum: string;
+  belopp: string;
+};
+
+type HomeChartProps = {
+  year: string;
+  setYear: (year: string) => void;
+  chartData: RowData[] | undefined;
+};
+
+export default function HomeChart({
+  year,
+  setYear,
+  chartData,
+}: HomeChartProps) {
+  const data = {
+    labels: chartData?.map((row) => row.datum) || [],
+    datasets: [
+      {
+        label: "Belopp",
+        data: chartData?.map((row) => row.belopp) || [],
+        backgroundColor: "rgb(8, 51, 68)",
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <div className="mx-auto w-4/5">
+        <Bar datasetIdKey="id" data={data} className="p-10 h-60" />
+      </div>
+      <label className="p-3" htmlFor="year">
+        Visa år:
+      </label>
+      <select
+        className="text-black p-2"
+        id="year"
+        onChange={(e) => setYear(e.target.value)}
+      >
+        <option value="">Select</option>
+        <option value="2022">2022</option>
+        <option value="2023">2023</option>
+        <option value="2024">2024</option>
+      </select>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,67 +1,26 @@
 "use client";
 
 import React, { useState } from "react";
-import "chart.js/auto";
-import { Bar } from "react-chartjs-2";
 import Card from "./Card";
+import HomeChart from "./HomeChart";
 import useFetch from "./hooks/useFetch";
 
-type RowData = {
-  id: number;
-  datum: string;
-  fil: string;
-  belopp: string;
-  inkomst_utgift: "Inkomst" | "Utgift";
-  land: string;
-  konto1: string;
-  konto2: string;
-  konto3: string;
-  titel: string;
-  kommentar: string;
-};
-
 export default function Home() {
-  const [selectedYear, setSelectedYear] = useState("2024");
-
-  const { fetchData } = useFetch(`api?q=${selectedYear}`);
-
-  const handleYearChange = (newYear: string) => {
-    setSelectedYear(newYear);
-  };
-
-  const chartData = {
-    labels: fetchData?.yearData.map((row: RowData) => row.datum) || [],
-    datasets: [
-      {
-        label: "Belopp",
-        data: fetchData?.yearData.map((row: RowData) => row.belopp) || [],
-        backgroundColor: "rgb(8, 51, 68)",
-      },
-    ],
-  };
+  const [year, setYear] = useState("2024");
+  const { fetchData } = useFetch(`api?q=${year}`);
 
   return (
-    <main className="items-center p-24 text-center">
+    <main className="items-center p-10 text-center">
       <div className="flex">
         <Card title="Inkomster" data={fetchData?.totalInkomst || 0} />
         <Card title="Utgifter" data={fetchData?.totalUtgift || 0} />
         <Card title="Resultat" data={fetchData?.resultat || 0} />
       </div>
-      <div className="mx-auto w-4/5">
-        <Bar datasetIdKey="id" data={chartData} className="p-10 h-60" />
-      </div>
-      <label htmlFor="year">Select a year:</label>
-      <select
-        className="text-black"
-        id="year"
-        value={selectedYear}
-        onChange={(e) => handleYearChange(e.target.value)}
-      >
-        <option value="">Select</option>
-        <option value="2022">2022</option>
-        <option value="2023">2023</option>
-        <option value="2024">2024</option>
-      </select>
+      <HomeChart
+        year={year}
+        setYear={setYear}
+        chartData={fetchData?.yearData}
+      />
     </main>
   );
 }
