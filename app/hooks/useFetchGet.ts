77cdd11c998e3e7ccd668e87733b72ca56dad527
RobertAngberg <124473123+RobertAngberg@users.noreply.github.com>
2024-02-28 02:13:25 +0100

    Fix API URL and update next.config.js

--- app/hooks/useFetchGet.ts
+++ app/hooks/useFetchGet.ts
@@ -7,7 +7,7 @@ const useFetchGet = (url: string) => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(`http://localhost:3000/` + url);
+        const response = await fetch(`https://localhost:3000/` + url);
         if (!response.ok) {
           throw new Error("Failed to fetch data");
         }
