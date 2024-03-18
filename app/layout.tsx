ef3656d4aedad0f3ca901de93ef1e8f84c10a337
RobertAngberg <124473123+RobertAngberg@users.noreply.github.com>
2024-03-18 13:50:10 +0100

    Auth magically working again

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./Navbar";
+import SessionProvider from "../SessionProvider";
+import { redirect } from "next/navigation";
+import { getServerSession } from "next-auth";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,17 +18,19 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  // const session = await getServerSession();
+  const session = await getServerSession();
 
-  // if (!session || !session.user) {
-  //   redirect("/api/auth/signin");
-  // }
+  if (!session || !session.user) {
+    redirect("/api/auth/signin");
+  }
 
   return (
     <html lang="en">
       <body className={inter.className}>
+        <SessionProvider session={session}>
         <Navbar />
         {children}
+        </SessionProvider>
       </body>
     </html>
   );
