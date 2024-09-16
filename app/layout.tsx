34832bc3b3196dbcffca6b87177e32fe060480b2
RobertAngberg <124473123+RobertAngberg@users.noreply.github.com>
2024-09-16 18:21:34 +0200

    Trying to remove auth cause its causing problems on Vercel, not locally

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,45 @@
+// import type { Metadata } from "next";
+// import { Inter } from "next/font/google";
+// import "./globals.css";
+// import { Navbar } from "./Navbar";
+// import SessionProvider from "../SessionProvider";
+// import { redirect } from "next/navigation";
+// import { getServerSession } from "next-auth";
+
+// const inter = Inter({ subsets: ["latin"] });
+
+// export const metadata: Metadata = {
+//   title: "Bokföringssystem, Robert Angberg",
+//   description: "Bokföringssystem, Robert Angberg",
+// };
+
+// export default async function RootLayout({
+//   children,
+// }: Readonly<{
+//   children: React.ReactNode;
+// }>) {
+//   const session = await getServerSession();
+
+//   if (!session || !session.user) {
+//     redirect("/api/auth/signin");
+//   }
+
+//   return (
+//     <html lang="en">
+//       <body className={inter.className}>
+//         <SessionProvider session={session}>
+//           <Navbar />
+//           {children}
+//         </SessionProvider>
+//       </body>
+//     </html>
+//   );
+// }
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "./Navbar";
-import SessionProvider from "../SessionProvider";
-import { redirect } from "next/navigation";
-import { getServerSession } from "next-auth";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,24 +48,16 @@ export const metadata: Metadata = {
   description: "Bokföringssystem, Robert Angberg",
 };
 
-export default async function RootLayout({
+export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const session = await getServerSession();
-
-  if (!session || !session.user) {
-    redirect("/api/auth/signin");
-  }
-
   return (
     <html lang="en">
       <body className={inter.className}>
-        <SessionProvider session={session}>
-          <Navbar />
-          {children}
-        </SessionProvider>
+        <Navbar />
+        {children}
       </body>
     </html>
   );
