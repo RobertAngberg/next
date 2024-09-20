22d35ab60f83c43fc23bad2a84154998513872c5
RobertAngberg <124473123+RobertAngberg@users.noreply.github.com>
2024-09-20 18:12:13 +0200

    Ok, database writing now works again... small change to Grundbok table on mobile

--- app/grundbok/Table.tsx
+++ app/grundbok/Table.tsx
@@ -2,29 +2,31 @@ import { TableRow } from "./TableRow";
 
 function Table({ historyData, handleRowClick, activeId, details }: TableProps) {
   return (
-    <table className="w-full m-auto md:w-3/4">
-      <thead className="text-lg bg-cyan-950">
-        <tr>
-          <th className="p-5 pl-10 rounded-tl-lg">ID</th>
-          <th className="p-5">Datum</th>
-          <th className="p-5 hidden md:table-cell">Fil</th>
-          <th className="p-5">Konto</th>
-          <th className="p-5">Belopp</th>
-          <th className="p-5 hidden md:table-cell pr-10 rounded-tr-lg">Kommentar</th>
-        </tr>
-      </thead>
-      <tbody>
-        {historyData.map((item) => (
-          <TableRow
-            key={item.transaktions_id}
-            item={item}
-            handleRowClick={handleRowClick}
-            activeId={activeId}
-            details={details}
-          />
-        ))}
-      </tbody>
-    </table>
+    <div className="w-full px-4 md:px-0"> {/* Added padding on small screens */}
+      <table className="w-full m-auto md:w-3/4">
+        <thead className="text-lg bg-cyan-950">
+          <tr>
+            <th className="p-5 pl-10 rounded-tl-lg">ID</th>
+            <th className="p-5">Datum</th>
+            <th className="p-5 hidden md:table-cell">Fil</th>
+            <th className="p-5">Konto</th>
+            <th className="p-5">Belopp</th>
+            <th className="p-5 hidden md:table-cell pr-10 rounded-tr-lg">Kommentar</th>
+          </tr>
+        </thead>
+        <tbody>
+          {historyData.map((item) => (
+            <TableRow
+              key={item.transaktions_id}
+              item={item}
+              handleRowClick={handleRowClick}
+              activeId={activeId}
+              details={details}
+            />
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
